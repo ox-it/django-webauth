3627a436dc0063d4dc75704f355f19a274448858
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-02-26 18:41:36 +0000

    Fixed typo.

--- django_webauth/views.py
+++ django_webauth/views.py
@@ -9,7 +9,7 @@ from django_conneg.http import HttpResponseSeeOther
 class LoginView(HTMLView):
     def get(self, request):
         username = request.META.get('REMOTE_USER')
-        elif not username:
+        if not username:
             raise ImproperlyConfigured('This view is supposed to set a REMOTE_USER environment variable')
 
         user = authenticate(username=username)
