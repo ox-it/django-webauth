5a5d5ec14ad7060d9eae40993e84bedca29b87c0
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-04-17 10:51:30 +0100

    Implemented login redirect checking, as cribbed from Django's login view (https://code.djangoproject.com/browser/django/trunk/django/contrib/auth/views.py). Fixes #2.

--- django_webauth/views.py
+++ django_webauth/views.py
@@ -1,12 +1,15 @@
+import urlparse
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login, logout, BACKEND_SESSION_KEY, REDIRECT_FIELD_NAME
 
 from django_conneg.views import HTMLView
 from django_conneg.http import HttpResponseSeeOther
 
 class LoginView(HTMLView):
+    redirect_field_name = REDIRECT_FIELD_NAME
+
     def get(self, request):
         username = request.META.get('REMOTE_USER')
         if not username:
@@ -15,7 +18,14 @@ class LoginView(HTMLView):
         user = authenticate(username=username)
         login(request, user)
 
-        return HttpResponseSeeOther(request.GET.get(REDIRECT_FIELD_NAME, settings.LOGIN_REDIRECT_URL))
+        redirect_to = request.GET.get(self.redirect_field_name, '')
+        netloc = urlparse.urlparse(redirect_to)[1]
+        if not redirect_to:
+            redirect_to = settings.LOGIN_REDIRECT_URL
+        elif netloc and netloc != request.get_host():
+            redirect_to = settings.LOGIN_REDIRECT_URL
+
+        return HttpResponseSeeOther(redirect_to)
 
 class LogoutView(HTMLView):
     def get(self, request):
