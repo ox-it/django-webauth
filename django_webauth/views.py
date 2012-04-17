33609b08bc464c9adf20235d7adbfc09ceb1c0a4
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-04-17 10:42:28 +0100

    LogoutView now displays "log out of Webauth" iff they logged in using Webauth in the first place. Also fixes #1.

--- django_webauth/views.py
+++ django_webauth/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, BACKEND_SESSION_KEY, REDIRECT_FIELD_NAME
 
 from django_conneg.views import HTMLView
 from django_conneg.http import HttpResponseSeeOther
@@ -15,10 +15,13 @@ class LoginView(HTMLView):
         user = authenticate(username=username)
         login(request, user)
 
-        return HttpResponseSeeOther(request.GET.get('next', settings.LOGIN_REDIRECT_URL))
+        return HttpResponseSeeOther(request.GET.get(REDIRECT_FIELD_NAME, settings.LOGIN_REDIRECT_URL))
 
 class LogoutView(HTMLView):
     def get(self, request):
+        context = {
+            'was_webauth': request.session.get(BACKEND_SESSION_KEY) == 'django_webauth.backends.WebauthBackend',
+        }
         logout(request)
         return self.render(request, context, 'webauth/logged_out')
 
