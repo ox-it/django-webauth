a1699cbe58afde1e85287223e50574bfd59989fd
David King <dave@davbo.org>
2012-07-20 17:15:14 +0100

    Updated setup.py to use find_packages +0.3

--- setup.py
+++ setup.py
@@ -1,52 +1,5 @@
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
-import os
+from setuptools import setup, find_packages
 
-#################################
-# BEGIN borrowed from Django    #
-# licensed under the BSD        #
-# http://www.djangoproject.com/ #
-#################################
-
-def fullsplit(path, result=None):
-    """
-Split a pathname into components (the opposite of os.path.join) in a
-platform-neutral way.
-"""
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('django_webauth'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-#################################
-# END borrowed from Django      #
-#################################
 
 # Idea borrowed from http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy
 install_requires, dependency_links = [], []
@@ -62,7 +15,7 @@ setup(
     description="WebAuth/LDAP integration for Django",
     author='Oxford University Computing Services',
     author_email='infodev@oucs.ox.ac.uk',
-    version='0.2',
+    version='0.3',
     license='BSD',
     url='https://github.com/oucs/django-webauth',
     long_description=open('README.rst').read(),
@@ -73,8 +26,7 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python',
                  'Topic :: Internet :: WWW/HTTP :: Dynamic Content'],
-    packages=['django_webauth'],
-    data_files=data_files,
+    packages=find_packages(),
     install_requires=install_requires,
     dependency_links=dependency_links,
 )
