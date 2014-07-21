6d045c4375c98c598180a19dc810e1982d813859
David King <dave@davbo.org>
2014-07-21 17:26:27 +0100

    Added comment on try/except imports

--- django_webauth/urls.py
+++ django_webauth/urls.py
@@ -1,6 +1,9 @@
+# Try/Except here as Django moved these modules from urls.defaults into urls in
+# Django 1.4 leaving proxy objects. These were completely removed in Django 1.6
 try:
     from django.conf.urls import patterns, url
 except ImportError:
+    # Django <1.4
     from django.conf.urls.defaults import patterns, url
 
 from . import views
